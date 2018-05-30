09556ba04410087293e95c1fbf233d4b2700a48f
shoko.nagata <shoko.nagata@rakuten.com>
2018-05-30 06:51:17 +0000

    validation

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -47,9 +47,17 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        $this->validate($request, [
+            'status' => 'required|max:10',   // add
+            'content' => 'required|max:191',
+        ]);
+
+
         $task = new Task;
+        $task->status = $request->status;    // add
         $task->content = $request->content;
         $task->save();
+
 
         return redirect('/');
     }
@@ -94,7 +102,12 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+         $this->validate($request, [
+            'status' => 'required|max:10',   // add
+            'content' => 'required|max:191',
+        ]);
         $task = Task::find($id);
+        $task->status = $request->status;  
         $task->content = $request->content;
         $task->save();
 
